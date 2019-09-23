ab18ebd5b125a243f5f85ac1375846c373106217
Grant Schumacher <grantschu99@gmail.com>
2019-09-22 19:18:00 -0600

    Generated rails model "Car" with parameters "make, model, vin, parts"

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_015944) do
+ActiveRecord::Schema.define(version: 2019_09_23_005138) do
 
   create_table "car_forms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cars_parts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
